64fd125db2735417afea0debaa6149881ec6cbbd
MarcinTomkiewicz <marc.tomk@gmail.com>
2023-07-12 10:40:04 +0200

    Added audio

--- src/App.js
+++ src/App.js
@@ -2,19 +2,25 @@ import "./App.css";
 import { CountdownTimer } from "./components/CountdownTimer/CountdownTimer";
 
 function App() {
-		return (
-		<div style={{display: "flex", flexDirection: "column", width: "100%", alignItems: "center", height: "100vh"}}>
-			<div className="cont">
-				<div className="spinner"></div>
+	return (
+		<>
+			<audio id="audio" loop autoplay>
+				<source src="%PUBLIC_URL%/music.mp3" type="audio/mpeg" />
+			</audio>
+			<div style={{ display: "flex", flexDirection: "column", width: "100%", alignItems: "center", height: "100vh" }}>
+				<div className="cont">
+					<div className="spinner"></div>
+				</div>
+				<div style={{ display: "flex", flexDirection: "column", width: "80%", alignItems: "center" }}>
+					<div className="header outline" data-content="Do woja idziecie za:">
+						Do woja idziecie za:
+					</div>
+					<div className="countdown__timer">
+						<CountdownTimer targetDate={"07/17/2023, 07:30"} />
+					</div>
+				</div>
 			</div>
-      <div style={{display: "flex", flexDirection: "column", width: "80%", alignItems: "center"}}>
-      <div className='header outline' data-content='Do woja idziecie za:'>Do woja idziecie za:</div>
-			<div className="countdown__timer">
-				<audio src={process.env.PUBLIC_URL + 'music.mp3'} autoPlay loop preload='auto'/>
-				<CountdownTimer targetDate={"07/17/2023, 07:30"} />
-			</div>
-      </div>
-		</div>
+		</>
 	);
 }
 
