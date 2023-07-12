f0dd339657c9b72c018e17167ed93878a037ad38
MarcinTomkiewicz <marc.tomk@gmail.com>
2023-07-12 10:55:08 +0200

    Sound working

--- src/App.js
+++ src/App.js
@@ -1,23 +1,24 @@
 import "./App.css";
+import sound from './music.mp3'
 import { CountdownTimer } from "./components/CountdownTimer/CountdownTimer";
 
 function App() {
 	return (
 		<>
-			<audio id="audio" loop autoplay>
-				<source src="%PUBLIC_URL%/music.mp3" type="audio/mpeg" />
-			</audio>
 			<div style={{ display: "flex", flexDirection: "column", width: "100%", alignItems: "center", height: "100vh" }}>
 				<div className="cont">
 					<div className="spinner"></div>
 				</div>
-				<div style={{ display: "flex", flexDirection: "column", width: "80%", alignItems: "center" }}>
+				<div style={{ display: "flex", flexDirection: "column", width: "80%", alignItems: "center", justifyContent: "space-between" }}>
 					<div className="header outline" data-content="Do woja idziecie za:">
 						Do woja idziecie za:
 					</div>
 					<div className="countdown__timer">
 						<CountdownTimer targetDate={"07/17/2023, 07:30"} />
 					</div>
+          <div>
+					<audio src={sound} autoPlay loop />
+          </div>
 				</div>
 			</div>
 		</>
