4b3f0bd21994b22064585c90058b2ec3651f5bc1
MarcinTomkiewicz <marc.tomk@gmail.com>
2023-07-17 09:12:10 +0200

    New date for counter. Music changed.

--- src/App.js
+++ src/App.js
@@ -1,27 +1,31 @@
 import "./App.css";
-import sound from './music.mp3'
+import sound from "./music.mp3";
 import { CountdownTimer } from "./components/CountdownTimer/CountdownTimer";
 
 function App() {
 	return (
-		<>
+		<div>
 			<div style={{ display: "flex", flexDirection: "column", width: "100%", alignItems: "center", height: "100vh" }}>
 				<div className="cont">
 					<div className="spinner"></div>
 				</div>
-				<div style={{ display: "flex", flexDirection: "column", width: "80%", alignItems: "center", justifyContent: "space-between" }}>
-					<div className="header outline" data-content="Do woja idziecie za:">
-						Do woja idziecie za:
+				<div className="header" style={{ display: "flex", flexDirection: "column", width: "85%", alignItems: "center", justifyContent: "space-around", height: "75%" }}>
+					<div>
+					<div>Stacja XXXIV: Pan Jezus idzie do wojska!</div>
+					<div>Wszyscy śpiewają: "Przyjedź mamo na przysięgę"</div>
 					</div>
-					<div className="countdown__timer">
-						<CountdownTimer targetDate={"07/17/2023, 07:30"} />
+					<div className="height" style={{ display: "flex", flexDirection: "column", width: "80%", alignItems: "center", justifyContent: "space-between", zIndex: 200 }}>
+						<div className="header outline" data-content="Do przysięgi pozostało:">
+							Do przysięgi pozostało:
+						</div>
+						<div className="countdown__timer">
+							<CountdownTimer targetDate={"08/12/2023, 09:30"} />
+						</div>
 					</div>
-          <div>
-					<audio src={sound} autoPlay loop />
-          </div>
+					<div><audio src={process.env.PUBLIC_URL + "/music.mp3"} autoPlay loop /></div>
 				</div>
 			</div>
-		</>
+		</div>
 	);
 }
 
